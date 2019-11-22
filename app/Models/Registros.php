9899ecd2df5c0e300e06689680544a9e76e126da
CERIMS <cerims@sagres.com.br>
2019-11-22 17:53:38 -0300

    [ADD] Verificacao para cauculo de horas

--- app/Models/Registros.php
+++ app/Models/Registros.php
@@ -15,14 +15,14 @@ class Registros extends Model
     public function registersForMoth()
     {
         return $this->query("SELECT r.id, r.data, r.entrada, r.entrada_intervalo, r.saida_intervalo, r.saida,
-        (SELECT TIMEDIFF(TIMEDIFF(r1.entrada_intervalo, r1.entrada),TIMEDIFF(r1.saida_intervalo, r1.saida))
+        (SELECT TIMEDIFF(verificar_valor(r1.saida, r1.entrada),verificar_valor(r1.saida_intervalo, r1.entrada_intervalo))
                 FROM registros as r1 WHERE r1.id = r.id) as saldo,
-       (SELECT TIMEDIFF(TIMEDIFF(r3.entrada_intervalo, r3.entrada),TIMEDIFF(r3.saida_intervalo, r3.saida)) FROM registros as r3
+       (SELECT TIMEDIFF(verificar_valor(r3.saida, r3.entrada),verificar_valor(r3.saida_intervalo, r3.entrada_intervalo)) FROM registros as r3
            WHERE r3.id = (SELECT max(id)-1 as id_anterior FROM registros ra WHERE ra.id = r.id ORDER BY ra.id DESC LIMIT 1)) as saldo_anterior,
         (SELECT TIMEDIFF(saldo, TIME('08:00:00')) FROM registros as r2 WHERE r2.id = r.id) as saldo_dia,
         (SELECT TIMEDIFF(saldo_anterior, TIME('08:00:00')) FROM registros as r3 WHERE r3.id = r.id) as saldo_diff,
         (SELECT ADDTIME(saldo_dia, saldo_diff) FROM registros AS r4 WHERE r4.id = r.id) as saldo_total
-        FROM registros as r WHERE YEAR(r.data) = YEAR(now()) AND MONTH(r.data) = MONTH(now()) ORDER BY r.id DESC;");
+        FROM registros as r /*WHERE YEAR(r.data) = YEAR(now()) AND MONTH(r.data) = MONTH(now()) ORDER BY r.id DESC*/;");
     }
 
     public function calcularTotalHoras(array $horas)
--- banco.sql
+++ banco.sql
@@ -25,4 +25,39 @@ CREATE TABLE registros (
     usuario_id INT UNSIGNED NOT NULL
 );
 
+-------------------------------------------------------------------------------------------------------------------
+-- PROCEDURE VERIFICAR VALORES PARA SUBITRAIR
+-------------------------------------------------------------------------------------------------------------------
 ALTER TABLE registros ADD CONSTRAINT fk_usuarios FOREIGN KEY (usuario_id) REFERENCES usuarios (id) ON DELETE CASCADE;
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `verificar_valor`$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `verificar_valor`(
+    v_id_registro INT,
+    v_entrada CHARACTER(8),
+    v_saida CHARACTER(8)
+)
+BEGIN
+    DECLARE TimeResult TIME;
+    DECLARE Result TIME;
+    DECLARE parts CHARACTER;
+
+    SELECT TIMEDIFF(v_saida, v_entrada) INTO TimeResult
+    FROM registros
+    WHERE id = v_id_registro;
+
+    SET parts = SUBSTRING_INDEX(SUBSTRING_INDEX(TimeResult, ':', 1), ' ', -1);
+
+    IF (parts < 0 || (LEFT(parts, 1) = '-')) THEN
+        SELECT TIMEDIFF(v_entrada, v_saida) INTO TimeResult
+        FROM registros
+        WHERE id = v_id_registro;
+    END IF;
+
+    SET Result = TimeResult;
+    SELECT Result;
+END $$
+
+DELIMITER ;
